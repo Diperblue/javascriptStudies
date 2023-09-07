104daee2802b9fb71adf6bcca1dbb40cb4582a7f
Diperblue <jorgeguimaraesdev@gmail.com>
2023-09-07 15:24:20 -0300

    class

---
+++ OOP/jsinfo/ex/ex.js
@@ -0,0 +1,9 @@
+class User{
+  construct(name) {
+    this.name = name
+  }
+
+  sayHi() {
+    return "Hi, my name's "+this.name
+  }
+}
---
+++ class/javascriptinfo/index.js
@@ -0,0 +1,8 @@
+function User(name, years) {
+				this.name = name;
+				this.years = years;
+};
+
+const d1p = new User("tralala", 19);
+
+console.log(d1p.name);
