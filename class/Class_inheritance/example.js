26c129650df74171f99c318866d251c0f0543369
Diperblue <jorgeguimaraesdev@gmail.com>
2023-09-11 11:52:08 -0300

    class

---
+++ class/Class_inheritance/example.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  myName() {
+    return "My name's "+this.name
+  }
+}
+
+class Policial extends Person {
+  gritar() {
+    consoel
+}
